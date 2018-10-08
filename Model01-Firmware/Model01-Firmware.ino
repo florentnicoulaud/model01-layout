d877338aacf6cb6a1860d442d9afbc9ce997eb6a
Florent Nicoulaud <fnicoulaud@norsys.fr>
2018-10-08 15:00:38 +0200

    feature: add ctrl+right and ctrl+left keys
    refactor: switch win key with tab/pgup key

--- Model01-Firmware/Model01-Firmware.ino
+++ Model01-Firmware/Model01-Firmware.ino
@@ -175,8 +175,8 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [COLEMAK] = KEYMAP_STACKED
   (Key_Backtick, Key_1, Key_2, Key_3, Key_4, Key_5, Key_Escape,
-    Key_LeftGui, Key_Q, Key_W, Key_F, Key_P, Key_G, Key_Tab,
-    Key_Tab,   Key_A, Key_R, Key_S, Key_T, Key_D,
+    Key_Tab, Key_Q, Key_W, Key_F, Key_P, Key_G, Key_Tab,
+    Key_LeftGui,   Key_A, Key_R, Key_S, Key_T, Key_D,
     Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, LCTRL(Key_LeftShift),
     Key_LeftAlt, Key_Backspace, Key_Enter, Key_Tab,
     ShiftToLayer(FUNCTION),
@@ -190,9 +190,9 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
 
     [FUNCTION] =  KEYMAP_STACKED
     (M(DEAD_TILDE),      Key_F1,  Key_F2,  Key_F3, Key_F4, Key_F5, Key_LEDEffectNext,
-    ___,  M(A_TREMA), M(A_CIRCONFLEXE), M(DEAD_TILDE), M(DEAD_AIGU), M(EURO_POUND), Key_F12,
-    Key_PageUp, M(A_GRAVE),  M(DEAD_GRAVE), Key_Home, Key_End, M(DEAD_TREMA),
-    Key_PageDown,  M(E_DANS_A), M(DEAD_CIRCONFLEXE),  M(C_CEDILLE), M(E_DANS_O), ___,  ___,
+    Key_PageUp,  M(A_TREMA), M(A_CIRCONFLEXE), M(DEAD_TILDE), M(DEAD_AIGU), M(EURO_POUND), Key_F12,
+    ___, M(A_GRAVE),  M(DEAD_GRAVE), Key_Home, Key_End, M(DEAD_TREMA),
+    ___, M(E_DANS_A), M(DEAD_CIRCONFLEXE),  M(C_CEDILLE), M(E_DANS_O), ___,  ___,
     ___, Key_Delete, ___, ___,
     ___,
 
@@ -200,7 +200,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
     Consumer_PlaySlashPause,    Key_LeftCurlyBracket, Key_RightCurlyBracket,     M(U_GRAVE),    M(U_TREMA), M(O_TREMA), Key_Pipe,
     Key_LeftBracket, M(E_GRAVE), M(E_AIGU), M(I_TREMA), M(O_CIRCONFLEXE), M(E_CIRCONFLEXE),
     Consumer_VolumeDecrement,   Key_RightBracket, Key_LeftArrow, Key_UpArrow, Key_DownArrow,  Key_RightArrow,    Key_Backslash,
-    ___, ___, ___, Key_Backspace,
+    ___, ___, LCTRL(Key_LeftArrow), LCTRL(Key_RightArrow),
     ___),
 
 
